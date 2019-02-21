6539a4e487cb398a398c49b23acf33496af74214
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-21 12:27:14 -0500

    Updates

---
+++ precache-manifest.b8164521e7f26c2bad79ed4979ba5fd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2067e78f8ca6198b4e9",
+    "url": "/react-todo/static/css/main.fca8846c.chunk.css"
+  },
+  {
+    "revision": "f2067e78f8ca6198b4e9",
+    "url": "/react-todo/static/js/main.f2067e78.chunk.js"
+  },
+  {
+    "revision": "713502522859ff23d42e",
+    "url": "/react-todo/static/js/runtime~main.71350252.js"
+  },
+  {
+    "revision": "bb94707ef3183ff076da",
+    "url": "/react-todo/static/js/2.bb94707e.chunk.js"
+  },
+  {
+    "revision": "bb472235e8d55c7ae5d3776c4573cc98",
+    "url": "/react-todo/index.html"
+  }
+];
